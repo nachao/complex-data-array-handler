7cfeaad63a83d53602aebc3395385f2f4e145f14
home <357586693@qq.com>
2016-01-10 02:15:45 +0800

    0.2完成，当前支持深度查询，以及新增一个查询路径属性

---
+++ narray-0.2.js
@@ -0,0 +1,432 @@
+
+/*
+ * nArray - JavaScript data management tools
+ *
+ * Copyright (c) 2016 Na Chao (https://github.com/nachao/nArray)
+ * Dual licensed under the MIT (MIT-LICENSE.txt) 
+ * and GPL (GPL-LICENSE.txt) licenses.
+ *
+ * Date: 2016-01-10 02:07:00 -0002 (Sun, 10 Jan 2016)
+ * Ver: 0.2
+ */
+
+function nArray( value, detail ){
+	
+	// If the context is global, return a new object
+	if ( window == this )
+		return new nArray(value, detail);
+}
+
+nArray.fn = nArray.prototype = {
+	version: '0.2',
+	author: 'Na Chao'
+}
+
+nArray.extend = nArray.fn.extend = function ( obj, prop ) {
+	if ( !prop ) { prop = obj; obj = this; }
+	for ( var i in prop ) obj[i] = prop[i];
+	return obj;
+};
+
+nArray.extend({
+	trim: function(t){
+		return typeof t == 'string' ? t.replace(/^\s+|\s+$/g, "") : t;
+	},
+	each: function(datas, fn){
+		var result = false,
+			j;
+		if ( datas.constructor == Object ) {
+			for ( j in datas ) {
+				if ( fn(j, datas[j]) ) {
+					result = true;
+					break;
+				}
+			}
+		}
+		else if ( datas.constructor == Array ) {
+			for ( j=0; j<datas.length; j++ ) {
+				if ( fn(j, datas[j]) ) {
+					result = true;
+					break;
+				}
+			}
+		}
+		return result;
+	},
+	indexOf: function(array, elt /*, from*/){
+		var len = array.length >>> 0;
+		var from = Number(arguments[1]) || 0;
+
+		from = (from < 0)
+			? Math.ceil(from)
+			: Math.floor(from);
+
+		if (from < 0)
+			from += len;
+
+		for (; from < len; from++) {
+			if (from in array && array[from] === elt)
+				return from;
+		}
+
+		return -1;
+	},
+	toKeys: function ( object ) {
+		var result = [];
+
+		if ( object && typeof object == 'object' ) {
+			for ( var key in object )
+				result.push(key);
+		}
+
+		return result;
+	},
+	toArray: function ( object, key ) {
+		var result = [];
+
+		if ( !object )
+			return;
+
+		for ( var i in object ) {
+			if ( typeof key == 'string' )
+				object[i][key] = i;
+			result.push(object[i]);
+		}
+
+		return result;
+	},
+	toObject: function ( array, key ) {
+		var result = {};
+
+		if ( !array )
+			return;
+
+		for ( var i=0; i<array.length; i++ ) {
+			if ( typeof key == 'string' && array[i][key] ) {
+				result[array[i][key]] = array[i];
+			}
+			else
+				result[i] = array[i];
+		}
+
+		return result;
+	},
+
+	// Will get characters converted to parameters
+	mateResolve: function ( value ) {
+
+		// 如果没有需要解析的字符串，则返回空数组
+		if ( typeof value != 'string' )
+			return [];
+
+		var modes = ['<=', '>=', '<', '>', '!=', '='],
+			results = [],
+			result,
+			mode,
+			item,
+			many,
+			key,
+			val,
+			i,
+			j;
+
+		value = value.toString().split(',');
+
+		// 遍历全部条件
+		for ( i=0; i<value.length; i++ ) {
+			mode = '=';
+			item = value[i];
+
+			// 匹配对应的条件符号
+			for ( j=0; j<modes.length; j++ ) {
+				if ( item.indexOf(modes[j]) >= 0 ) {
+					mode = modes[j];
+					break;
+				}
+			}
+
+			item = item.split(mode);
+
+			// 键值
+			if ( item.length > 1 ) {
+				key = nArray.trim(item[0]);
+				val = nArray.trim(item[1]);
+			}
+			else {
+				key = null;
+				val = nArray.trim(item[0]);
+			}
+
+			// 一个条件对应多个值
+			if ( val.indexOf('|') >= 0 ) {
+				many = val.split('|');
+				result = [];
+				for ( k=0; k<many.length; k++ ) {
+					result.push({
+						key: key,
+						val: nArray.trim(many[k]),
+						mode: mode
+					});
+				}
+			}
+			else {
+
+				// 条件格式，及默认数据
+				result = {
+					key: key,
+					val: val,
+					mode: mode
+				};
+			}
+
+			if ( result.constructor == Array )
+				results = results.concat(result);
+			else
+				results.push(result);
+		}
+
+		return results;
+	},
+
+	// Loop all data, return the match result
+	mateFor: function (method, datas, parameter, whole ) {
+		var result = [],
+			params = nArray.mateResolve(parameter);
+
+		// 初始化查询路径
+		result.constructor.prototype.$path = [];
+
+		// 深度查询方式
+		nArray.mateDepth(method, params, datas, result);
+
+		return result;
+	},
+
+	// Depth matching, return to the match to the data, as well as path.
+	// Return {array}
+	mateDepth: function ( method, params, datas, result, paths ) {
+		var path = [],
+			i;
+
+		if ( !paths )
+			paths = [];
+
+		for ( var i=0; i<paths.length; i++ )
+			path.push(paths[i]);
+
+		if ( datas.constructor == Object ) {
+			path.push(datas);
+			if ( nArray.mateData(method, params, datas) ) {
+			result.push(path[path.length-1]);
+				result.constructor.prototype.$path.push(path);
+			}
+			for ( i in datas ) {							
+				if ( nArray.mateDepth(method, params, datas[i], result, path) )
+					break;
+			}
+		}
+		else if ( datas.constructor == Array ) {
+			path.push(datas);
+			if ( nArray.mateData(method, params, datas) ) {
+			result.push(path[path.length-1]);
+				result.constructor.prototype.$path.push(path);
+			}
+			for ( i=0; i<datas.length; i++ ) {							
+				if ( nArray.mateDepth(method, params, datas[i], result, path) )
+					break;
+			}
+		}
+		else if ( nArray.mateData(method, params, datas) ) {
+			result.push(path[path.length-1]);
+			result.constructor.prototype.$path.push(path);
+		}
+	},
+
+	// According to the specified method, determine whether the data meet the conditions
+	// Return {boolean}
+	mateData: function ( method, params, datas ) {
+		var result = false,
+			rights = 0,
+			param,
+			i,
+			j;
+
+		// 循环所以条件
+		for ( i=0; i<params.length; i++ ) {
+			param = params[i];
+
+			// 如果数据有键值
+			if ( param.key ) {
+
+				// 判断数据是否对应键值和值
+				if ( typeof datas == 'object' && nArray.mateKeys(datas, param) ) {
+
+					// 循环对象或数组全部数据，直到有匹配中的值为止
+					nArray.each(datas, function(i, data){
+						return result = nArray.mateValues(data, param, method);
+					});
+				}
+			}
+
+			// 如果数据没有键值，但有数据值
+			else if ( param.val ) {
+				if ( typeof datas == 'object' ) {
+					nArray.each(datas, function(i, data){
+						return result = nArray.mateValues(data, param, method);
+					});
+				}
+				else {
+					result = nArray.mateValues(datas, param, method);
+				}
+			}
+
+			if ( result ) {
+
+				// 判断是否为多条件严格匹配
+				if ( method.indexOf('whole') >= 0 )
+					result = ( ++rights == params.length );
+				else
+					break;
+			}
+		}
+
+		return result;
+	},
+
+	// To the corresponding two values, select the matching method
+	// Param {object|array} data
+	// Return {boolean}
+	mateKeys: function ( data, param ) {
+		return nArray.each(data, function(key){
+			return String(key).indexOf(param.key) >= 0;
+		});
+	},
+
+	// To the corresponding two values, select the matching method
+	// Return {boolean}
+	mateValues: function ( value, param, method ) {
+		var result = false;
+			
+		param.val = param.val.toLocaleLowerCase();
+		value = value.toString().toLocaleLowerCase();
+
+		if ( param.mode == '=' )
+			result = method.indexOf('full') >= 0 ?
+				value === param.val :
+				value.indexOf(param.val) >= 0;
+
+		else if ( param.mode == '!=' ) 
+			result = method.indexOf('search') >= 0 ?
+				value.indexOf(param.val) < 0 :
+				value !== param.val;
+
+		else if ( param.mode == '>' )
+			result = value > param.val;
+
+		else if ( param.mode == '>=' )
+			result = value >= param.val;
+
+		else if ( param.mode == '<' )
+			result = value < param.val;
+
+		else if ( param.mode == '<=' )
+			result = value <= param.val;
+
+		return result;
+	},
+
+	// Whether the specified conditions are determined for the 'full match' data
+	mateFull: function ( datas, parameter, whole ) {
+		var method = 'full';
+
+		// 判断是否为严格查询
+		if ( whole )
+			method = 'whole ' + method;
+
+		return parameter ? nArray.mateFor(method, datas, parameter) : datas;
+	},
+
+	// Whether the specified condition is a 'fuzzy match' of the data
+	mateSearch: function ( datas, parameter, whole ) {
+		var method = 'search';
+
+		// 判断是否为严格搜索
+		if ( whole )
+			method = 'whole ' + method;
+
+		return parameter ? nArray.mateFor(method, datas, parameter) : datas;
+	}
+});
+
+// 对数据进行完整匹配查询，支持条件判断：<,>,>=,<=,=,!=
+Array.prototype.$get = function ( value, whole ) {
+	return nArray.mateFull(this, value, whole);
+}
+
+// 对数据进行模糊查询，支持条件判断：<,>,>=,<=,=,!=
+Array.prototype.$search = function ( value, whole ) {
+	return nArray.mateSearch(this, value, whole);
+},
+
+// 对数据进行过滤，提取指定需要的值
+Array.prototype.$fetch = function ( value ) {
+	var result = [],
+		factor = [],
+		item;
+
+	value = value ? value.split(',') : [];
+
+	for ( var i=0; i<value.length; i++ )
+		factor.push(nArray.trim(value[i]));
+
+	if ( factor.length ) {
+
+		// 循环全部数据
+		for ( var i=0; i<this.length; i++ ) {
+			item = {};
+			for ( var j=0; j<factor.length; j++ ) {
+				if ( nArray.indexOf(nArray.toKeys(this[i]), factor[j]) >= 0 )
+					item[factor[j]] = this[i][factor[j]];
+			}
+			if ( nArray.toKeys(item).length )
+				result.push(item);
+		}
+	}
+	else {
+		result = this;
+	}
+
+	return result;
+},
+
+// 对数据进行批量修改或扩展
+Array.prototype.$update = function ( value ) {
+	if ( typeof value != 'undefined' ) {
+
+		// 循环全部数据
+		for ( var i=0; i<this.length; i++ ) {
+
+			// 确保数据类型相同
+			if ( this[i].constructor == value.constructor ) {
+				if ( typeof value == 'object' )
+					for ( var j in value )
+						this[i][j] = value[j];
+				else
+					this[i] = value;
+			}
+		}
+	}
+	return this;
+}
+
+// 对数据进行去重复处理（只作用于元素为非object、array、null、underfind）
+Array.prototype.$unique = function () {
+	var result = [];
+
+	for ( var i=0; i<this.length; i++ ) {
+		if ( result.indexOf(this[i]) < 0 )
+			result.push(this[i]);
+	}
+
+	return result;
+}
