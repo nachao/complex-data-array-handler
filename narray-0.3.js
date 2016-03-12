ea6a50132a39cf8e24480339e5a5e70b5f004799
home <357586693@qq.com>
2016-03-12 13:46:33 +0800

    版本升级至：0.4（提交代码）  1、添加新的查看格式：详细查询，之前的字符串查询方式任然可用。和之前不同的是，之前传入的是字符串，现在可以传入对象数组，详细使用见下面的示例。（重要）
    * 2、优化开发版的注释以及代码的优化和简化；

--- narray-0.3.js
+++ narray-0.3.js
@@ -19,8 +19,16 @@
 
 	var 
 
-		// 全部条件符号
-		caseSign = ['<=', '>=', '<', '>', '!=', '='];
+		/**
+		 *  全部条件符号
+		 *
+		 *  说明：处于对编程习惯考虑的原因“等于匹配符号”添加了一个双等于号（==），
+		 *  它和单等于号（=）完全一样。
+		 *  因为此工具已经有被项目使用，所有不删除 = 符号，两个都可以正常使用。  
+		 *
+		 *  提示：strict（精确匹配）参数只有条件符号为：'==' 是有效； 
+		 */
+		caseSign = ['<=', '>=', '<', '>', '!=', '==', '=' ];
 
 
 	/************************************
@@ -177,70 +185,160 @@
 			return result;
 		},
 
-		// 拆分条件字符串
-		parseCondition: function ( value ) {
-			var result = [];
-
-			// 如果没有需要解析的字符串，则返回空数组
-			value = value ? value.toString().split(',') : [];
-
-			// 遍历全部条件
-			each(value, function(i, val){
-				var sign = NArray.getInquiry(val),
-					item = val.split(sign),
-					key = item[0],
-					value = item[1];
-
-				if ( !sign && !value ) {
-					value = key || '';
-					key = '';
-				}
-
-				result = result.concat(NArray.getCondition(key, value.split('|'), sign));
-			});
-
-			return result;
-		},
-
-		// 判断字符串中是否有条件符号，如果有则返回
-		// @return {string}
+		/***********************************************************
+		 * 判断字符串中是否有条件符号，如果有则返回，
+		 * 如果不给判断值，则返回默认条件符号 ‘==’。
+		 *
+		 * @param {string} value = 判断此值是否在匹配符号中，选填
+		 * @return {string}
+		 **/
 		getInquiry: function ( value ) {
 			var result = '';
 
-			each(caseSign, function(i, sign){
-				if ( value.indexOf(sign) >= 0 ) {
-					result = sign;
-					return true;
-				}
-			});
+			if ( value ) {
+				each(caseSign, function(i, sign){
+					if ( value.indexOf(sign) >= 0 ) {
+						result = sign;
+						return true;
+					}
+				});
+			}
 
 			return result;
 		},
 
-		// 根据给出的一个主键、值和条件符号返回一个或多个对象
-		getCondition: function ( key, value, mode ) {
+		/**
+		 * 遍历条件
+		 * 根据一个主键、值和条件符号返回一个或多个对象
+		 *
+		 * #对于没有的参数会给出默认值。
+		 *
+		 * @param {object} cond = 包含以下参数
+		 * @param {string} key
+		 * @param {string|number|boolean|null|undefined} value
+		 * @param {string} mode
+		 * @param {boolean} enable
+		 * @param {boolean} strict = 是否严格匹配值，默认false。说明：true 等于 ===；false 为 ==；
+		 *
+		 * @return {array}
+		 **/
+		eachCondition: function ( cond ) {
 			var result = [];
 
-			if ( value.constructor != Array )
-				value = [value];
+			// 如果是字符串类型
+			if ( typeof cond.value == 'string' )
+				cond.value = cond.value.split('|');	// 转换为数组格式
 
-			each(value, function(i, val){
+			// 其他非数组类型
+			else if ( !cond.value || cond.value.constructor != Array ) {
+				cond.value = [cond.value];			// 转换为数组格式
+			}
+
+			// 遍历全部此条件，处理一个条件对应多个值的情况
+			each(cond.value, function(i, val){
+
+				// 格式化字符串
+				if ( typeof cond.key == 'string' )
+					cond.key = cond.key.trim();
+
+				if ( typeof val == 'string' )
+					val = val.trim();
+
 				result.push({
-					key: key == '*' ? '' : key.trim(),
-					val: val == '*' ? '' : val.trim(),
-					mode: mode || '='
+					key: cond.key == '*' ? '' : cond.key,
+					val: val == '*' ? '' : val,
+					mode: cond.mode || '==',
+					enable: cond.enable || true,
+					strict: cond.strict || false
 				});
 			});
 
 			return result;
 		},
 
-		// 根据指定的：条件，数据，方式，是否完整匹配，进行深度匹配
-		// 方式包括：
-		// 对数据进行完整匹配查询，method='get'；
-		// 对数据进行模糊查询：method='search';
-		// 支持条件：
-		// <,>,>=,<=,=,!=
+		/**
+		 * 拆分条件字符串
+		 *
+		 * @param {string} cond
+		 * @return {array}
+		 **/
+		parseByString: function ( cond ) {
+			var result = [];
+
+			// 如果没有需要解析的字符串，则返回空数组
+			var values = cond ? cond.toString().split(',') : [];
+
+			// 遍历全部条件
+			each(values, function(i, value){
+				var mode = NArray.getInquiry(value),
+					item = value.split(mode),
+					key = item[0],
+					val = item[1];
+
+				// 在没有条件符号的情况下，则匹配全部键值
+				if ( !mode && !val ) {
+					val = key || '';
+					key = '';
+				}
+
+				// 遍历和初始条件参数
+				result = result.concat(NArray.eachCondition({
+					key: key, 
+					value: val.split('|'), 
+					mode: mode
+				}));
+			});
+
+			return result;
+		},
+
+		/**
+		 * 解析详细（对象）条件
+		 *
+		 * @param {array|object} value
+		 * @return {array}
+		 **/
+		parseByObject: function ( value ) {
+			var result = [];
+
+			// 如果条件为单个对象，则将其转换为数组
+			if ( value.constructor == Object )
+				value = [value];
+
+			// 遍历全部条件
+			each(value, function(i, val){
+				result = result.concat(NArray.eachCondition(val));
+			});
+
+			return result;
+		},
+
+		/**
+		 *  判断条件类型和根据相关配置对条件进行解析
+		 **/
+		parseCondition: function ( value ) {
+			var result = [];
+
+			// 字符串条件
+			if ( typeof value == 'string' )
+				result = NArray.parseByString(value);
+
+			// 详细对象数组条件
+			else if ( value.constructor == Array || value.constructor == Object ) {
+				result = NArray.parseByObject(value);
+			}
+
+			return result;
+		},
+
+		/***************************************************************
+		 *  根据指定的：条件，数据，方式，是否完整匹配，进行深度匹配
+		 *  方式包括：
+		 *  对数据进行完整匹配查询，method='get'；
+		 *  对数据进行模糊查询：method='search';
+		 *  支持条件：
+		 *  <,>,>=,<=,=,!=
+		 **/
 		mateEnter: function ( datas, value, method, whole ) {
 			var result = [];
 
@@ -347,31 +445,38 @@
 
 		// 根据条件，判断值是否匹配
 		// Return {boolean}
-		mateValues: function ( value, param, method ) {
+		mateValues: function ( value, cond, method ) {
 			var result = false,
-				strValue = value ? value.toString().toLocaleLowerCase() : '',
-				strVal = param.val.toString().toLocaleLowerCase();
+				dataValue = value,
+				condVal = cond.val;
+
+			// 如果是普通匹配，则将数据值和条件值都转换为字符串，其转换为小写
+			if ( !cond.strict ) {
+				dataValue = dataValue ? dataValue.toString().toLocaleLowerCase() : '';
+				condVal = condVal ? condVal.toString().toLocaleLowerCase() : '';
+			}
 
 			// 对象或函数数据不能进行匹配
 			// 如果数据内容的值为 null，则按照空字符串来匹配
 			if ( ['object', 'function'].indexOf(typeof value) >= 0 && value != null )
 				return result;
 
-			switch ( param.mode ) {
+			switch ( cond.mode ) {
 				case '=' :
+				case '==' :
 					result = method.indexOf('get') >= 0 ?
-						strValue === strVal :
-						strValue.indexOf(strVal) >= 0;
+						dataValue === condVal :
+						dataValue.indexOf(condVal) >= 0;
 					break;
 
 				case '!=' :
 					result = method.indexOf('search') >= 0 ?
-						strValue.indexOf(strVal) < 0 :
-						strValue !== strVal;
+						dataValue.indexOf(condVal) < 0 :
+						dataValue !== condVal;
 					break;
 
 				default :
-					result = eval('"' + value + '"' + param.mode + '"' + param.val + '"'); 
+					result = eval('"' + value + '"' + cond.mode + '"' + cond.val + '"'); 
 			}
 
 			return result;
